8dc45dd922e4bdbb0b7a4125477d907d1b70ea36
Ryan Gregg <rgregg@users.noreply.github.com>
2016-08-14 16:48:59 -0700

    New stuff

---
+++ TemperatureRecorderConsoleApp/OneWireProbeReader.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TemperatureRecorderConsoleApp
+{
+    public static class OneWireProbeReader
+    {
+        public static string DevicesDirectory = "/sys/bus/w1/devices";
+
+        public static string[] EnumerateDevices()
+        {
+            var devicesDir = new DirectoryInfo(DevicesDirectory);
+            if (!devicesDir.Exists)
+            {
+                Console.WriteLine(string.Format("Unable to locate device directory: {0}", devicesDir.FullName));
+                return new string[0];
+            }
+
+            var knownDevices = devicesDir.EnumerateDirectories("28*");
+            return (from d in knownDevices select d.Name).ToArray();
+        }
+
+        public static TemperatureData GetValueFromDevice(string deviceId)
+        {
+            var deviceDir = new DirectoryInfo(Path.Combine(DevicesDirectory, deviceId));
+            if (!deviceDir.Exists)
+            {
+                throw new Exception(string.Format("Unable to locate directory for device {0}", deviceId));
+            }
+
+            var sourceDataFile = deviceDir.GetFiles("w1_slave").FirstOrDefault();
+            if (null == sourceDataFile)
+            {
+                throw new Exception(string.Format("Unable to file w1_slave for device {0}", deviceId));
+            }
+
+            string dataText = null;
+            DateTimeOffset instance = DateTimeOffset.UtcNow;
+            using (var sourceDataReader = sourceDataFile.OpenText())
+            {
+                dataText = sourceDataReader.ReadToEnd();
+            }
+
+            string temptext = dataText.Split(new string[] { "t=" }, StringSplitOptions.RemoveEmptyEntries)[1];
+            double temp_C = double.Parse(temptext) / 1000;
+
+            return new TemperatureData(instance, deviceId, temp_C);
+        }
+
+        /// <summary>
+        /// Reads {measurements} values from the probe over {seconds} and returns the average temperature value recorded.
+        /// </summary>
+        /// <param name="deviceId"></param>
+        /// <param name="seconds"></param>
+        /// <param name="measurements"></param>
+        /// <returns></returns>
+        public static TemperatureData GetAverageValueFromDevice(string deviceId, double seconds, double measurements)
+        {
+            int sleepTimer = (int)((seconds / measurements) * 1000.0);
+            List<TemperatureData> recordedValues = new List<TemperatureData>();
+            for (int i = 0; i < measurements; i++)
+            {
+                var data = OneWireProbeReader.GetValueFromDevice(deviceId);
+                if (null != data)
+                    recordedValues.Add(data);
+                System.Threading.Thread.Sleep(sleepTimer);
+            }
+
+            if (recordedValues.Count > 0)
+            {
+                double averageTemperature = 0;
+                foreach (var data in recordedValues)
+                {
+                    averageTemperature += data.TemperatureC;
+                }
+                return new TemperatureData(DateTimeOffset.UtcNow, deviceId, averageTemperature / recordedValues.Count);
+            }
+
+            return null;
+        }
+    }
+
+
+    public class TemperatureData
+    {
+        public DateTimeOffset InstanceDateTime { get; private set; }
+        public string DeviceIdentifier { get; private set; }
+        public double TemperatureC { get; private set; }
+        public double TemperatureF
+        {
+            get
+            {
+                return TemperatureC * 9.0 / 5.0 + 32.0;
+            }
+        }
+
+        public TemperatureData(DateTimeOffset time, string device, double tempC)
+        {
+            this.InstanceDateTime = time;
+            this.DeviceIdentifier = device;
+            this.TemperatureC = tempC;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "{3}: Device {0} reports temperature: {1}C, {2}F", 
+                DeviceIdentifier,
+                TemperatureC,
+                TemperatureF,
+                InstanceDateTime);
+        }
+    }
+}
--- TemperatureRecorderConsoleApp/Program.cs
+++ TemperatureRecorderConsoleApp/Program.cs
@@ -8,49 +8,36 @@ namespace TemperatureRecorderConsoleApp
     {
         static void Main(string[] args)
         {
-            DirectoryInfo devicesDir = new DirectoryInfo("/sys/bus/w1/devices");
-            if (!devicesDir.Exists)
+
+            int pauseBetweenReadings = 5000;
+            if (args.Length > 0)
             {
-                Console.WriteLine(string.Format("Unable to locate device directory: {0}", devicesDir.FullName));
+                var newPauseValue = Int32.Parse(args[0]);
+                if (newPauseValue > 0)
+                    pauseBetweenReadings = newPauseValue * 1000;
+            }
+
+            var probes = OneWireProbeReader.EnumerateDevices();
+            if (probes.Length == 0)
+            {
+                Console.WriteLine("Unable to locate any devices. Aborting.");
                 return;
             }
 
             while (true)
             {
-                ReadProbeData(devicesDir);
-                System.Threading.Thread.Sleep(1000);
+                foreach (var probe in probes)
+                {
+                    var data = OneWireProbeReader.GetAverageValueFromDevice(probe, 5, 10);
+                    if (null != data)
+                    {
+                        Console.Write(data.ToString());
+                    }
+                }
+                System.Threading.Thread.Sleep(pauseBetweenReadings);
             }
         }
 
-        private static void ReadProbeData(DirectoryInfo devicesDir)
-        {
-            foreach (var deviceDir in devicesDir.EnumerateDirectories("28*"))
-            {
-                try
-                {
-                    var sourceDataFile = deviceDir.GetFiles("w1_slave").FirstOrDefault();
-                    if (null == sourceDataFile)
-                    {
-                        Console.WriteLine(string.Format("Unable to file w1_slave for device {0}", deviceDir.Name));
-                        continue;
-                    }
-
-                    string dataText = null;
-                    using (var sourceDataReader = sourceDataFile.OpenText())
-                    {
-                        dataText = sourceDataReader.ReadToEnd();
-                    }
-
-                    string temptext = dataText.Split(new string[] { "t=" }, StringSplitOptions.RemoveEmptyEntries)[1];
-                    double temp_C = double.Parse(temptext) / 1000;
-                    double temp_F = temp_C * 9.0 / 5.0 + 32.0;
-                    Console.WriteLine(string.Format("Device {0} reports temperature: {1}C, {2}F", deviceDir.Name, temp_C, temp_F));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(string.Format("Error reading data from probe {0}: {1}", deviceDir.Name, ex.Message));
-                }
-            }
-        }
+     
     }
 }
